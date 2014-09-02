1e10e5a42f79ad4abf5ac8e89811d1e136b24181
daniel877111 <dat218@lehigh.edu>
2014-09-02 00:00:01 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////
+//Brian chen
+//HW01
+//September 2, 2014
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("   -----------       ");
+    System.out.println("   | Welcome |       ");
+    System.out.println("   -----------       ");
+    System.out.println("  ^  ^  ^  ^  ^  ^   ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+    System.out.println("|-D--A--T--2--1--8-|       ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /  ");
+    System.out.println("  v  v  v  v  v  v         ");
+    System.out.println("I can't believe that I am in college right now");
+    System.out.println("Time flew by but I'm loving Lehigh.           ");
+    
+    //test/testest
+  }
+}
