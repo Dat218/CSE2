e849fb3d5a281f69e7ddf35b05ecdbcd10e51896
daniel877111 <dat218@lehigh.edu>
2014-11-15 22:24:16 +0000

    Updated Arrays

--- lab11/Arrays.java
+++ lab11/Arrays.java
@@ -7,7 +7,7 @@ public class Arrays {
 //main method required for every program
     public static void main(String [] args) {
         Scanner scan = new Scanner ( System. in );
-        System.out.println("Enter 10 ints: ");
-        int [] userInputs = scan.nex
+        System.out.println("Enter 10 ints with a space between each integer: ");
+        int[] list = new int [10];
     }
-}
+}///////////////////////////////////////
