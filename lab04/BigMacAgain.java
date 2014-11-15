09afa813393d3da251c596fd337fa92a7ee362ab
daniel877111 <dat218@lehigh.edu>
2014-11-15 22:34:02 +0000

    Updated BigMacAgain

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -3,7 +3,7 @@
 //September 19, 2014
 //CSE 002 Lab 04
 //
-//  define a class
+//define a class
 import java.util.Scanner;
 import java.text.DecimalFormat;
 public class BigMacAgain {
@@ -11,34 +11,34 @@ public class BigMacAgain {
 //main method required for every java program
   public static void main(String[] args) {
     
-  Scanner myScanner;
-  myScanner = new Scanner( System.in );
-  DecimalFormat d = new DecimalFormat("##.##");
+  Scanner myScanner; //declare
+  myScanner = new Scanner( System.in ); //construct
+  DecimalFormat d = new DecimalFormat("##.##"); //declaration and construction
   
   System.out.print("Enter the number of Big Macs: ");
-  if(myScanner.hasNextInt()) {
+  if(myScanner.hasNextInt()) {  //ensures that user inputs digit
     int nBigMacs = myScanner.nextInt();
       if (nBigMacs >= 0) {
         System.out.println("You ordered " +nBigMacs+
         " for a cost of " +nBigMacs+ "x2.22 = " 
         +d.format(nBigMacs*2.22));
-      }  
-      else {
+      }  //Calculates cost of big mac
+      else { 
         System.out.println("It is impossible to order a negative number of Big Macs.");
       return;
-      }
-    System.out.print("Do you want an order of fries (Y/y/N/n)? ");
-      String answer=myScanner.next();
+      } //Can not enter a negative number
+    System.out.print("Do you want an order of fries (Y/y/N/n)? ");  //Asks user wheter or not they want fries, use must enter one of the following variables
+      String answer=myScanner.next(); 
       if (answer.equals("Y") || answer.equals("y")) {
         System.out.println("You ordered fries at a cost of $2.15");
         System.out.println("The total cost of the meal is $" +d.format((nBigMacs*2.22)+2.13));
-      }
+      }  //Calculates total cost if user wants fries
       else if (answer.equals("N") || answer.equals("n")) {
       System.out.println("The total cost of the meal is $" +d.format(nBigMacs*2.22));
-      }
+      }  //calculates total cost if user does not want fries
         else {
           System.out.println("You did not enter one of 'y', 'Y', 'N', 'n'");
-        }
+        } //result of user does not enter one of the following variables
   }
           else{
            System.out.println("You did not enter an int!");
