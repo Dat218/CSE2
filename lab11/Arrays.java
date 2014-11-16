e137eedd7fb8f9f004e10f49fefcc18cb589dff8
daniel877111 <dat218@lehigh.edu>
2014-11-16 03:51:19 +0000

    Updated Array

--- lab11/Arrays.java
+++ lab11/Arrays.java
@@ -7,7 +7,32 @@ public class Arrays {
 //main method required for every program
     public static void main(String [] args) {
         Scanner scan = new Scanner ( System. in );
-        System.out.println("Enter 10 ints with a space between each integer: ");
+        System.out.print("Enter 10 ints with a space between each integer: ");
         int[] list = new int [10];
+            for (int i=0; i<10; i++) {
+                list[i] = scan.nextInt();
+            }
+        int highestEntry = 0;
+        int lowestEntry = list[1];
+            for (int i=0; i<10; i++) {
+                if (list[i] > highestEntry) {
+                    highestEntry = list[i];
+                }
+                if (list[i] < lowestEntry) {
+                    
+                    lowestEntry = list[i];
+                }
+            }
+        int totalSum = 0;
+            for (int i=0; i<10; i++) {
+                totalSum = totalSum + list[i];
+            }
+        for (int i=0; i<10; i++) {
+            System.out.print("\t" + list[i]);
+            System.out.println("\t" + list[10-i-1]);
+        }
+            System.out.println("The highest entry is " + highestEntry);
+            System.out.println("The lowest entry is " + lowestEntry);
+            System.out.println("The sum is " + totalSum);
     }
 }
