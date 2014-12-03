12ed7a4905c7d1eb5988de564bfc501a413d698d
daniel877111 <dat218@lehigh.edu>
2014-12-03 03:13:07 +0000

    Practice

--- Practice/Practice5.java
+++ Practice/Practice5.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
 public class Practice5 {
 	public static void main(String[] args) {
-		System.out.print("ENTER AN INT: ");
-		Scanner scan = new Scanner( System.in );
-		int firstInput = scan.nextInt();
-		System.out.print("ENTER AN INT > "+firstInput+": ");
-		int secondInput = scan.nextInt();
-			while (secondInput<=firstInput) {
-				System.out.print("YOU ENTERED AN INT <="+firstInput+"; TRY AGAIN: ");
-				scan.next();
+		int[] myList1 = {1,10,3,7};
+		int[] myList2 = {2,3,4,5,10,6};
+		int[] myList3 = new int[6];
+		int counter = 0;
+		for (int i=0; i<myList1.length; i++) {
+			for (int j=0; j<myList2.length; j++) {
+				if (myList1[i]==myList2[j]) {
+					counter++;
+					myList3[counter] = myList1[i];
+				}
 			}
-		sum(firstInput,secondInput);
-	}
-	public static void sum(int firstInput, int secondInput) {
-		int m = firstInput;
-		int lastValue = 0;
-		for (int i = firstInput; i<secondInput+1; i++) {
-			if (i == firstInput) {
-				System.out.print(i);
-			}
-			else {
-				System.out.print("+"+i);
-			}
-			lastValue = lastValue + i;
 		}
-		System.out.print(" = " +lastValue);
+		myList3[0] = counter;
+		for (int i = 0; i<myList3.length; i++) {
+			System.out.println("myList["+i+"] is: " + myList3[i]);
+		}
 	}
 }
